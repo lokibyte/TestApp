59206a7d7e1dcc5caca949e2908437b55973f5aa
tech savy <msg2loki@gmail.com>
2024-05-30 22:22:46 +0530

    Update register.page.ts

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -13,6 +13,36 @@ export class RegisterPage implements OnInit {
   ngOnInit() {
   }
   doRegister(){
-    this.router.navigate(['/home']);
+    // this.router.navigate(['/home']);
+  }
+  locationChange(){
+    this.validateRegistration();
+  }
+  nameChange(){
+    this.validateRegistration();
+  }
+  onSelect(role:any,idx:any){
+    this.selectedFltr = idx;
+    role.selected = true;
+    this.selectedroles.push(role);
+    this.validateRegistration();
+  }
+  onisRoleChange(evt:any){
+    console.info(evt.target.checked);
+    if(evt.target.checked){
+      this.isanyrolechekced = true;
+    }else{
+      this.isanyrolechekced = false;
+    }
+    this.validateRegistration();
+  }
+  validateRegistration(){
+    console.info(this.selectedroles,)
+    console.info("isDisabled",this.isDisabled,this.selectedroles,this.isanyrolechekced,this.name,this.location);
+    if( (this.selectedroles.length > 0 || this.isanyrolechekced) && this.name.length > 0 && this.location.length > 0){
+      this.isDisabled = false;
+    }else{
+      this.isDisabled = true;
+    }
   }
 }
